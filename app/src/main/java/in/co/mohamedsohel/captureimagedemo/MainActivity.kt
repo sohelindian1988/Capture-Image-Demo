6421d401072157668a17e8a1683a4185437e70cc
Mohamedsohel <sohel.3s@gmail.com>
2024-09-09 08:52:28 +0530

    Updated

---
+++ app/src/main/java/in/co/mohamedsohel/captureimagedemo/MainActivity.kt
@@ -0,0 +1,43 @@
+package `in`.co.mohamedsohel.captureimagedemo
+
+import android.content.Intent
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var imageView: ImageView
+    private lateinit var buttonCaptureImage: Button
+    private lateinit var launcher:ActivityResultLauncher<Intent>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        imageView = findViewById(R.id.imageView)
+        buttonCaptureImage = findViewById(R.id.buttonCaptureImage)
+
+launcher=registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+    result->
+    if(result.resultCode == RESULT_OK)
+    {
+        val bundle: Bundle? =result.data?.extras
+        val bitmap= bundle?.get("data") as Bitmap
+        imageView.setImageBitmap(bitmap)
+    }
+}
+
+        buttonCaptureImage.setOnClickListener {
+            val intent= Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            launcher.launch(intent)
+        }
+
+    }
+}
